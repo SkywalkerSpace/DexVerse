2cccd71525cf1ae05c36fd4d85623526f81500ad
SkywalkerSpace <dzmbbs@qq.com>
2026-07-22 18:23:01 +0800

    openpi-client

--- conda.sh
+++ conda.sh
@@ -1,5 +1,7 @@
 conda activate dexverse
 
+pip install -e packages/openpi-client
+
 python scripts/zero_agent.py --task=Dexverse-OpenLaptop-v0 --enable_cameras --num_envs=1
 
 python scripts/pi05_agent.py --task=Dexverse-OpenLaptop-v0 --num_envs=1
